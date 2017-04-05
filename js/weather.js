24108dd68c3ba34de58404b79a667b641ff2a692
keri19372 <keri19372@gmail.com>
2017-04-05 16:56:50 +0300

    display actual data! Added img

--- js/weather.js
+++ js/weather.js
@@ -10,17 +10,10 @@ var	weather_state_name = document.querySelector("#weather_state_name");
 var	wind_speed = document.querySelector("#wind_speed");
 
 var shortWeather = weather.consolidated_weather[0];
-
-	humidity.innerText = shortWeather.humidity;
-	predictability.innerText = shortWeather.predictability;
-	the_temp.innerText = shortWeather.the_temp;
-	weather_state_name.innerText = shortWeather.weather_state_name;
-	wind_speed.innerText = shortWeather.wind_speed;
+var weatherIconsePlaceholders = document.querySelectorAll("img");
 
 
 
-cityName.innerText = weather.title;
-time.innerText = weather.time;
 
 var select = document.querySelector("#city");
 
@@ -29,9 +22,18 @@ select.onchange = function() {
 
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
-    	if (this.readyState == 4 && this.status == 200) {  		
-       
-   		 } 
+    	if (this.readyState == 4 && this.status == 200) {
+       		var response = JSON.parse(this.responseText); 
+       		console.log(response); 
+
+       		humidity.innerText = response.current.humidity;
+			the_temp.innerText = response.current.temp_c;
+			weather_state_name.innerText = response.current.condition.text;
+			wind_speed.innerText = response.current.wind_kph;
+
+			cityName.innerText = response.location.name;
+			time.innerText = response.location.localtime;     
+   		 }
 	};
 	xhttp.open("GET", "https://intense-beach-78744.herokuapp.com/?city= " + townName, true);
 	xhttp.send();	
